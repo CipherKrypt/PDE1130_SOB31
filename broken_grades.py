e849051e86d75ef81632d4ced5e04d1a3d3f7b98
CipherKrypt <SS4334@live.mdx.ac.uk>
2021-11-20 17:15:44 +0400

    Changed reserved keyword sum as Sum for the variable

--- broken_grades.py
+++ broken_grades.py
@@ -26,11 +26,11 @@ exam_two = int(input("Input exam grade two: "))
 exam_3 = int(input("Input exam grade three: "))
 
 grades = [exam_one,exam_two,exam_three]
-sum = 0
+Sum = 0
 for grade in grade:
-  sum = sum + grade
+  Sum = Sum + grade
 
-avg = sum / len(grdes)
+avg = Sum / len(grdes)
 
 if avg >= 90:
     letter_grade = "A"
